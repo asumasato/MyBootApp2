1de111508ee2b4d3c9f9f14458dead0d02ae12e0
佐藤 飛真 <200770@jc-21.jp>
2023-06-01 14:15:45 +0900

    コミットメッセージ

---
+++ src/main/java/jp/te4a/spring/boot/myapp2/HelloController.java
@@ -0,0 +1,22 @@
+package jp.te4a.spring.boot.myapp1;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+* Hello world!
+*
+*/
+@RestController
+public class HelloController
+{
+@RequestMapping("/taro")
+public String taro() {
+return "Taro desu!";
+}
+	
+	@RequestMapping("/")
+	public String index() {
+		return "Hello, Spring Boot!";
+	}
+}
